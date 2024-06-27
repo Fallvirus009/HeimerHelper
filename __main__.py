b89b0438144a848e2870e8204252c7615453fe8d
Daniel Elkoni <44883449+Fallvirus009@users.noreply.github.com>
2024-06-27 12:40:48 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 from helpers import get_static_player_data, get_match_history, get_static_live_data, getLivePlayerData
 
-summoner = 'Fallvirus009'
+summoner = 'SUMM_NAME'
 
 print(get_static_player_data(summoner))
 print(get_static_player_data(summoner)['puuid'])
 print(get_match_history(summoner, 5))
-#print(getMatchData(get_match_history(summoner, 5)[0]))
+print(getMatchData(get_match_history(summoner, 5)[0]))
 print(getLivePlayerData())
